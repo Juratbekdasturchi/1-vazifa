8fdd385ae511332d81f9c9f3bb81fef4d7572d87
Juratbekdasturchi <juratbekgithubabduraimov@gmail.com>
2024-03-01 11:11:58 +0500

    Hello worldcha

---
+++ +loyiha01/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <header>
+      <h1> Assalomu alaykum </h1>
+      <div className = "container">
+        <a href="#"> Start Bootstrap </a>
+        <ul>
+            <li> <a href="#">Home</a></li>
+            <li> <a href="#">About</a></li>
+            <li> <a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </header>
+    <section id = "homeSection">
+      <div className = "container"></div>
+    </section>
+   </div>
+  );
+}
